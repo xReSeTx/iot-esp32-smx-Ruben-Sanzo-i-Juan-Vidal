ef186005f4295fc5d95a4adbd086af3d07776caa
juanvidalmoreno602-rgb <juanvidalmoreno602@gmail.com>
2026-05-27 11:15:24 +0200

    Create Issue5.ino
    closes #15

---
+++ Projecte Final - GreenFarm/src/Issue5.ino
@@ -0,0 +1,3 @@
+L'unic error que hem trobat es que habiam posat la temperatura d'activació del reg era massa alta (28 graus). 
+  Per tant hem hagut de baixar aquesta temperatura als 14 graus perque així si que puguessim arribar a aquesta temperatura tocant el sensor LM35. 
+D'aquesta manera si hem pogut corroborar que l'activació de reg era correcte.
