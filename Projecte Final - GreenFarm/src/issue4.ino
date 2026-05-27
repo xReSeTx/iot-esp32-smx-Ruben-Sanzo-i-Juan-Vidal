8c667e95e9f8c2b83bf541991105512b64023980
xReSeTx <156317199+xReSeTx@users.noreply.github.com>
2026-05-27 10:49:48 +0200

    Create issue4.ino
    closes #13

---
+++ Projecte Final - GreenFarm/src/issue4.ino
@@ -0,0 +1,11 @@
+Circuit dissenyat i provat correctament.
+
+Connexions:
+- LM35 VCC → 3.3V
+- LM35 Vout → Pin 35
+- LM35 GND → GND
+- Relé → Pin 5
+- LED verd → Pin 18
+- LED vermell → Pin 19
+
+Sistema funcionant correctament a 12.49°C.
